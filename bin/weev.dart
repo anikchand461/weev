e3c8bafe0fef1260df6771329cb419d92364a64b
im-AyanRoy <ayanroysince2003official@gmail.com>
2026-01-23 01:08:06 +0530

    Initial commit

---
+++ bin/weev.dart
@@ -0,0 +1,5 @@
+import 'package:weev/weev.dart' as weev;
+
+void main(List<String> arguments) {
+  print('Hello world: ${weev.calculate()}!');
+}
---
+++ test/weev_test.dart
@@ -0,0 +1,8 @@
+import 'package:weev/weev.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
